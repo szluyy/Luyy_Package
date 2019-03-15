eba8b3c3e2345438a0dcf9d6c8a698d655fde249
szluyy <48274637+szluyy@users.noreply.github.com>
2019-03-15 14:54:09 +0800

    init commit

--- photoview/src/main/java/com/luyy/photoview/PhotoPickerActivity.java
+++ photoview/src/main/java/com/luyy/photoview/PhotoPickerActivity.java
@@ -4,10 +4,11 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
-public class PhotoPicker extends AppCompatActivity{
+public class PhotoPickerActivity extends AppCompatActivity{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photopicker);
+
     }
 }
